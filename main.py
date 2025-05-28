255eb38c7513ffa0ed7728ad7f9214af9b8d3771
realthrongler <fjack6677@gmail.com>
2025-05-28 14:30:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+#importing libraries
+import pygame
+from pygame import locals
 
+pygame.init()
+
+running = True
+caption = ""
+
+screen_size = (500, 500)
+screen = pygame.display.set_mode(screen_size, flags=pygame.RESIZABLE)
+
+pygame.display.set_caption(caption)
+
+while running:
+    
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            running = False
+
+pygame.quit()
