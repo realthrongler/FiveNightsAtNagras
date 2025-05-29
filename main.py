dd479bfd6d0b8014e3e83286263e125c750b94a4
realthrongler <fjack6677@gmail.com>
2025-05-29 13:12:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,90 @@
-#importing libraries
+# Programmer(s):
+# Date:
+# Description:
+
 import pygame
-from pygame import locals
+from pygame import *
+from pygame.sprite import *
 
+# define colour constants
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+### ADD ANY OTHER COLOUR CONSTANTS HERE ###
+
+# define system constants
+FPS = 60
+WIDTH = 640
+HEIGHT = 480
+BGCOLOUR = BLACK ### CHANGE AS NEEDED ###
+
+# initialize pygame, create window, start the clock
 pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
+### ADD YOUR SPRITE CLASSES HERE ###
+
+#class ImageSprite(Sprite):
+#    def __init__(self, x, y, filename):                    # NEW sprite at (x,y)
+#        Sprite.__init__(self)                              # init the Sprite object
+#        self.image = image.load(filename).convert()        # loads the image from filename as the sprite  
+#        self.rect = self.image.get_rect()                  # creates the rectangle around the sprite
+#        self.rect.center = (x//2,y//2)                           
+
+#    # semi-optional part
+#    def update(self):
+#        ### ADD MOVEMENT MODIFIERS HERE ###
+#        self.rect.x += 5
+#        self.rect.y -= 2
+#        self.rect.center = (x//2,y//2)  
+        
+
+### ADD SPRITE INSTANCES HERE ###
+
+#sprite = ImageSprite(x, y, "INSERT_IMAGE_FILENAME.jpg")
+
+# group sprites
+allSprites = pygame.sprite.Group()
+
+# game loop
 running = True
-caption = ""
-
-screen_size = (500, 500)
-screen = pygame.display.set_mode(screen_size, flags=pygame.RESIZABLE)
-
-pygame.display.set_caption(caption)
-
 while running:
-    
+    # keep loop running at the right speed 
+    clock.tick(FPS)
+    # process input (events)
     for event in pygame.event.get():
-        if event == pygame.QUIT:
+        if event.type == QUIT:
             running = False
-
+        
+        ### ADD ANY OTHER EVENTS HERE (KEYS, MOUSE, ETC.) ###
+        
+    # game loop updates (including movement)
+    ### ADD ANY GAME LOOP UPDATES HERE ###
+    
+    # check for keypresses
+    #keys = pygame.key.get_pressed()
+    
+    #if keys[K_LEFT]:
+        # do something
+    #if keys[K_RIGHT]:
+        # do something 
+    
+    # game loop drawing
+    ### ADD ANY GAME LOOP DRAWINGS HERE ###
+    
+    # background fill
+    screen.fill(BGCOLOUR)
+    
+    # update position of sprites
+    
+    
+    # render sprites on screen
+    allSprites.draw(screen)
+    
+    # ***AFTER*** drawing everthing, flip (update) the display
+    pygame.display.flip()
+    
 pygame.quit()
