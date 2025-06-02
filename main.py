8b8db1782fd92ef498a7ea7de36b0067802e8f73
realthrongler <fjack6677@gmail.com>
2025-06-02 13:32:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -81,8 +81,102 @@ image = pygame.image.load("ValveIntro.png").convert_alpha()
 image_rect = image.get_rect(center=(1280 // 2, 720 // 2))
 
 clock = pygame.time.Clock()
-running = True
 
+def MenuScreen():
+    pass
+
+def NewGamePressed():
+    pass
+
+def SettingsPressed():
+    pass
+
+def StoryIntroduction():
+    pass
+
+def DrawOfficeScreen():
+    pass
+
+def LookAtDoor():
+    pass
+
+def RunToDoor():
+    pass
+
+def RunToComputer():
+    pass
+
+def RunToWindow():
+    pass
+
+def OpenCameras():
+    pass
+
+def SwitchCameras(camera):
+    pass
+
+def CloseCameras():
+    pass
+
+def Flashlight():
+    pass
+
+def LoganMovement():
+    pass
+
+def MaxMovement():
+    pass
+
+def NoahMovement():
+    pass
+
+def NagraMovement():
+    pass
+
+def LoganJumpscare():
+    pass
+
+def MaxWindowBreak():
+    pass
+
+def MaxJumpscare():
+    pass
+
+def NoahAppear():
+    pass
+
+def ComputerShutoff():
+    pass
+
+def ComputerPowerOn():
+    pass
+
+def NagraJumpscare():
+    pass
+
+def CloseDoor():
+    pass
+
+def ShutOffMusic():
+    pass
+
+def PlayMusic():
+    #DJ toenail here
+    pass
+
+def GameOverScreen():
+    pass
+
+def RetryButtonPressed():
+    pass
+
+def NightWin():
+    pass
+
+def NightStart():
+
+        
+running = True
 while running:
     current_ticks = pygame.time.get_ticks()
     elapsed_seconds = (current_ticks - start_ticks) / 1000.0
