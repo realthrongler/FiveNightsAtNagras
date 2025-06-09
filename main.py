40457d03d4d7c1d736c93ae713d0d9050330f13c
realthrongler <fjack6677@gmail.com>
2025-06-09 13:56:05 -0400

    more stuff

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ actions = {"CanWindow": False,
 "ComputerOff": False,
 "State": "MENU", #Variable for determining player location, including "WINDOW", "DOOR", "DESK", "CAMERA", "MENU", "WIN", "LOSS"
 "Night": 1,
-"Camera": 1, #Defaults to Logan's hall
+"Camera": 1, #Defaults to Logan's hall, 2 is storage room, 3 is just outside storage room, 4 is bench, 5 is mr.nagra chair
 "StartTime": 0.00} #Float for storing the time the night started in milliseconds
 
 animatronicHandler = {
@@ -261,8 +261,19 @@ def OpenCameras():
     actions["CanDoor"] = False
     actions["CanWindow"] = False
 
-def SwitchCameras(camera):
-    pass
+def UpCameras():
+    camera = actions["Camera"]
+    if camera + 1 == 6:
+        actions["Camera"] = 1
+    else:
+        actions["Camera"] += 1
+
+def DownCameras():
+    camera = actions["Camera"]
+    if camera - 1 == 0:
+        actions["Camera"] = 5
+    else:
+        actions["Camera"] -= 1
 
 def CloseCameras():
     actions["CanDisableMusic"] = False
